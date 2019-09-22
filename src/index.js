f52632010bf03276e059bfea4170e6a8a0f4015f
AnastasiaSK <nastassya.karachun@gmail.com>
2019-09-22 23:53:48 +0300

    feat: implement  decode function

--- src/index.js
+++ src/index.js
@@ -38,8 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = expr;
+    const countOfSub = str.length/10;
+    const subs = [];
+    for (let i = 0; i < countOfSub; i++) {
+        subs.push(str.substring(0,10));
+        str = str.substring(10); 
+    }
+    for (let index = 0; index < subs.length; index++) {
+        let element = subs[index];
+        element = element.replace(/00/g,'');
+        element = element.replace(/10/g,'.');
+        element = element.replace(/11/g,'-');
+        if (element === "**********") {
+            subs[index] = element.replace('**********',' ');
+        } else {
+            subs[index] = MORSE_TABLE[element];      
+        }  
+        
+    }
+    const resultStr = subs.join('');
+    return resultStr;
+
 }
+
 
 module.exports = {
     decode
